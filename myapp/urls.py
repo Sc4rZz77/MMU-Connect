66fd9de68cdded97a32080591629ec36649b9fd3
Sc4rZz77 <shaarving658@gmail.com>
2025-05-09 19:16:15 +0800

    ai chat(beta)

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from .views import CustomLoginView
+from .views import ai_chat
+
 
 urlpatterns = [
     path("test", views.test, name="test"),
@@ -18,6 +20,8 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('api/chat/', ai_chat, name='ai_chat'),
+
 
 
 
