11ab54354082a89a44ec92cb779c68d880632aa4
Sc4rZz77 <shaarving658@gmail.com>
2025-05-30 17:14:25 +0800

    slight changes

--- MMUConnect/settings.py
+++ MMUConnect/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = 'django-insecure--zvx^18#848zxe+8+iqemxf$%y*9n_i#-bseu%-#7t!&@5!-sc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'bae4-2001-e68-5455-3514-bd15-ebfa-409a-8bac.ngrok-free.app',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
