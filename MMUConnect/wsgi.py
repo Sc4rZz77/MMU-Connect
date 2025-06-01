48bc93ad8d8c7daaf5b8fb716e599727cd09676e
Sc4rZz77 <shaarving658@gmail.com>
2025-06-02 02:27:59 +0800

    wsgi

--- MMUConnect/wsgi.py
+++ MMUConnect/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+from django.core.asgi import get_asgi_application
 import os
-
-from django.core.wsgi import get_wsgi_application
+from whitenoise import ASGIStaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MMUConnect.settings')
-
-application = get_wsgi_application()
+application = ASGIStaticFilesHandler(get_asgi_application())
