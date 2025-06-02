c19f45f25411f4ce460df5a27af7a50e987b51f7
Sc4rZz77 <shaarving658@gmail.com>
2025-06-02 17:59:30 +0800

    port chage

--- MMUConnect/settings.py
+++ MMUConnect/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_URL = '/media/'  # Optional (for URL patterns)
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
