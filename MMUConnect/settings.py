50532f6cb4d4a6452e602c462e78509ae7ead5de
Sc4rZz77 <shaarving658@gmail.com>
2025-06-02 18:49:40 +0800

    databse

--- MMUConnect/settings.py
+++ MMUConnect/settings.py
@@ -81,13 +81,12 @@ WSGI_APPLICATION = 'MMUConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
